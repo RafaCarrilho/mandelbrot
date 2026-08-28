056de5f7c9db3233d766a37e138d5df325f2012f
Rafael Carrilho <rac4@cesar.school>
2026-08-28 18:39:00 -0300

    Criado a lógica BASICA de um cálculo de mandelbrot (após aprender como faz esse cálculo)

---
+++ escape.c
@@ -0,0 +1,19 @@
+#include "escape.h"
+
+int escape (double cr, double ci, int max_iteracoes){
+    int iteracoes =0;
+    double zr=0; 
+    double zi=0;
+    
+    while (zr*zr + zi*zi < 4 && iteracoes < max_iteracoes){
+    
+        double zr_novo = zr*zr - zi*zi + cr;    
+        double zi_novo = 2*zr*zi + ci;   
+        zr = zr_novo;
+        zi = zi_novo; 
+
+        iteracoes++; 
+    }
+    return iteracoes;
+    
+}
